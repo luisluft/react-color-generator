12def674abc2b4d04a66286985eaa1758e844547
luisluft <luberluft@gmail.com>
2021-09-21 07:27:10 -0400

    add raw UI elements

--- src/App.js
+++ src/App.js
@@ -4,7 +4,36 @@ import SingleColor from "./SingleColor";
 import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>;
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("event :", event);
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="#f15025"
+            value={color}
+            onChange={(event) => setColor(event.target.value)}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>list goes here</h4>
+      </section>
+    </>
+  );
 }
 
 export default App;
