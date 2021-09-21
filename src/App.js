d5eadc96f6fc10f8bad35ac6e66c30d492bb060f
luisluft <luberluft@gmail.com>
2021-09-21 08:07:38 -0400

    add default value for the color generator

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import SingleColor from "./SingleColor";
 import Values from "values.js";
 
 function App() {
+  let numberOfShades = 10;
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#f15025").all(numberOfShades));
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     try {
-      let colors = new Values(color).all(10);
+      let colors = new Values(color).all(numberOfShades);
       setList(colors);
       setError(false);
     } catch (error) {
